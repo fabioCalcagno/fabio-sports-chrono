7665d77ee4a5ff44456500fe1e282679759ce972
FabioCalcagno <calcagno.it@gmail.com>
2023-11-08 17:30:56 -0300

    inicial commit ionic with tabs and android ok

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.fabiocalcagnodev',
+  appName: 'Sports Chrono',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
